d64cdc5d3db098b57ce3efed8f877bba40cc65c9
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-08-11 23:28:54 +0700

    refactor(pages): code format template login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="container">
     <div class="w-1/3">
-      <h1 class="border-b-2 border-t-2 border-white mb-5 text-3xl text-white">AndRoom</h1>
+      <h1 class="border-b-2 border-t-2 border-white mb-5 text-3xl text-white">
+        AndRoom
+      </h1>
       <div>
         <div>
-            <label for="email" class="block text-white text-left pb-2">Email</label>
-            <input class="p-2 w-full" v-model="user.email" name="email" type="email" placeholder="email">
+          <label for="email" class="block text-white text-left pb-2"
+            >Email</label
+          >
+          <input
+            class="p-2 w-full"
+            v-model="user.email"
+            name="email"
+            type="email"
+            placeholder="email"
+          />
         </div>
         <div>
-            <label for="password" class="block text-white text-left pb-2 pt-2">Password</label>
-            <input class="p-2 w-full" v-model="user.password" name="password" type="password" placeholder="password">
+          <label for="password" class="block text-white text-left pb-2 pt-2"
+            >Password</label
+          >
+          <input
+            class="p-2 w-full"
+            v-model="user.password"
+            name="password"
+            type="password"
+            placeholder="password"
+          />
         </div>
-        <button @click="doLogin()" class="block mt-8 border w-full border-white text-white p-2">Login</button>
-        <p class="p-2 text-sm text-green-100">don't have account? <nuxt-link to="/register">register</nuxt-link></p>
+        <button
+          @click="doLogin()"
+          class="block mt-8 border w-full border-white text-white p-2"
+        >
+          Login
+        </button>
+        <p class="p-2 text-sm text-green-100">
+          don't have account? <nuxt-link to="/register">register</nuxt-link>
+        </p>
       </div>
     </div>
   </div>
