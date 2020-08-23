39fe57518b1b8e05af0b2e93da1f818f31d12d20
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-08-23 16:45:04 +0700

    refactor(pages): implement toast, better handling error, implement watch on login pages

--- pages/login.vue
+++ pages/login.vue
@@ -7,36 +7,42 @@
       <div>
         <div>
           <label for="email" class="block text-white text-left pb-2"
-            >Email</label
+          >Email</label
           >
           <input
             class="p-2 w-full"
             v-model="user.email"
             name="email"
             type="email"
+            required
+            @keypress.enter="doLogin"
             placeholder="email"
           />
         </div>
         <div>
           <label for="password" class="block text-white text-left pb-2 pt-2"
-            >Password</label
+          >Password</label
           >
           <input
             class="p-2 w-full"
             v-model="user.password"
             name="password"
             type="password"
+            required
+            @keypress.enter="doLogin"
             placeholder="password"
           />
         </div>
         <button
           @click="doLogin()"
+          :disabled="button.login.disabled"
           class="block mt-8 border w-full border-white text-white p-2"
         >
-          Login
+          {{ button.login.label }}
         </button>
         <p class="p-2 text-sm text-green-100">
-          don't have account? <nuxt-link to="/register">register</nuxt-link>
+          don't have account?
+          <nuxt-link to="/register">register</nuxt-link>
         </p>
       </div>
     </div>
@@ -48,27 +54,54 @@ export default {
   data() {
     return {
       title: 'Login',
+      button: {
+        login: {
+          label: "Login",
+          disabled: false
+        }
+      },
+      options: {
+        toast: {
+          fullWidth: true,
+          theme: "bubble",
+          duration: 5000,
+          position: "bottom-center"
+        }
+      },
       user: {
         password: null,
         email: null,
       },
     }
   },
+  watch: {
+    "button.login.disabled": function (value) {
+      if(value){
+        this.button.login.label = "Please wait...";
+      }else{
+        this.button.login.label = "Login";
+      }
+    }
+  },
   middleware: 'authenticated',
   methods: {
-    doLogin: function () {
-      this.$fireAuth
-        .signInWithEmailAndPassword(this.user.email, this.user.password)
-        .then((data) => {
-          this.$auth.$storage.setLocalStorage('isLogin', true)
-          this.$auth.$storage.setLocalStorage('user', data.user, true)
-          this.$router.push({
-            path: '/',
-          })
+    async doLogin() {
+
+      this.button.login.disabled = true;
+
+      try {
+        let data = await this.$fireAuth.signInWithEmailAndPassword(
+          this.user.email, this.user.password)
+        this.$auth.$storage.setLocalStorage('isLogin', true)
+        this.$auth.$storage.setLocalStorage('user', data.user, true)
+        await this.$router.push({
+          path: '/',
         })
-        .catch(function (error) {
-          throw error
-        })
+
+      } catch (error) {
+        this.button.login.disabled = false;
+        this.$toast.error(error.message, this.options.toast);
+      }
     },
   },
 }
