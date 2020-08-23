098b1e05257de78853e3bfa1512b73d1ae6c014e
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-08-23 22:21:29 +0700

    refactor(pages): index improvement

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-  test</div>
+  <div class="container">
+    <div class="w-full">
+      <p class="p-2 text-xs text-white w-full" v-for="(value, name) of user">{{ name }} : {{ value }}</p>
+      <button
+        @click="doLogout()"
+        class="mt-8 border border-white text-white p-2"
+      >
+        Logout
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      'test' : 'oke'
+      test: 'oke',
     }
-  }
+  },
+  computed: {
+    user: function () {
+      console.log(this.$auth.$storage.getLocalStorage('user'))
+      return this.$auth.$storage.getLocalStorage('user')
+    }
+  },
+  middleware: 'guest',
+  methods: {
+    doLogout: function () {
+      this.$fireAuth.signOut().then(() => {
+        this.$auth.$storage.setLocalStorage('isLogin', false)
+        this.$auth.$storage.setLocalStorage('user', null)
+        this.$router.push({
+          path: '/login',
+        })
+      })
+    },
+  },
 }
 </script>
 
