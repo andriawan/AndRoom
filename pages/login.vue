341e05916850fc7d9731cee30d77286702aea231
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-08-23 22:22:19 +0700

    refactor(pages): improvement login via phone number firebase

--- pages/login.vue
+++ pages/login.vue
@@ -5,45 +5,100 @@
         AndRoom
       </h1>
       <div>
-        <div>
-          <label for="email" class="block text-white text-left pb-2"
-          >Email</label
-          >
-          <input
-            class="p-2 w-full"
-            v-model="user.email"
-            name="email"
-            type="email"
-            required
-            @keypress.enter="doLogin"
-            placeholder="email"
-          />
+        <div v-show="options.login.mode == options.login.DEFAULT">
+          <div>
+            <label for="email" class="block text-white text-left pb-2"
+            >Email</label
+            >
+            <input
+              class="p-2 w-full"
+              v-model="user.email"
+              name="email"
+              id="email"
+              type="email"
+              required
+              @keypress.enter="doLogin"
+              placeholder="email"
+            />
+          </div>
+          <div>
+            <label for="password" class="block text-white text-left pb-2 pt-2"
+            >Password</label
+            >
+            <input
+              class="p-2 w-full"
+              v-model="user.password"
+              name="password"
+              type="password"
+              id="password"
+              required
+              @keypress.enter="doLogin"
+              placeholder="password"
+            />
+          </div>
         </div>
+
         <div>
-          <label for="password" class="block text-white text-left pb-2 pt-2"
-          >Password</label
-          >
-          <input
-            class="p-2 w-full"
-            v-model="user.password"
-            name="password"
-            type="password"
-            required
-            @keypress.enter="doLogin"
-            placeholder="password"
-          />
+          <div v-show="options.login.mode == options.login.PHONE">
+            <label for="phone" class="block text-white text-left pb-2"
+            >Phone</label
+            >
+            <input
+              class="p-2 w-full"
+              v-model="user.phone"
+              id="phone"
+              name="phone"
+              type="tel"
+              @keypress.enter="doLogin"
+              placeholder="+628xxxxx"
+            />
+          </div>
+          <div id="recaptcha-container"></div>
+          <div v-show="options.login.mode == options.login.OTP">
+            <label for="otp" class="block text-white text-left pb-2"
+            >Phone</label
+            >
+            <input
+              class="p-2 w-full"
+              v-model="user.phone"
+              id="otp"
+              name="otp"
+              type="number"
+              maxlength="7"
+              @keypress.enter="doLogin"
+              placeholder="please enter OTP"
+            />
+          </div>
         </div>
         <button
           @click="doLogin()"
+          v-show="options.login.mode == options.login.DEFAULT || options.login.mode == options.login.PHONE"
           :disabled="button.login.disabled"
           class="block mt-8 border w-full border-white text-white p-2"
         >
           {{ button.login.label }}
         </button>
-        <p class="p-2 text-sm text-green-100">
-          don't have account?
-          <nuxt-link to="/register">register</nuxt-link>
-        </p>
+        <button
+          @click="confirmOTP()"
+          v-show="options.login.mode == options.login.OTP"
+          :disabled="button.otp.disabled"
+          class="block mt-8 border w-full border-white text-white p-2"
+        >
+          {{ button.otp.label }}
+        </button>
+        <div class="flex">
+          <div class="w-1/2">
+            <p class="p-2 text-sm text-green-100">
+              don't have account?
+              <nuxt-link to="/register">register</nuxt-link>
+            </p>
+          </div>
+          <div class="w-1/2">
+            <p @click="changeLoginMode()" class="cursor-pointer p-2 text-sm text-green-100">
+              {{ options.login.label.text }}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -57,7 +112,13 @@ export default {
       button: {
         login: {
           label: "Login",
-          disabled: false
+          disabled: false,
+          DEFAULT: "Login",
+          OTP: "Send OTP"
+        },
+        otp: {
+          label: "Confirm",
+          disabled: false,
         }
       },
       options: {
@@ -66,11 +127,27 @@ export default {
           theme: "bubble",
           duration: 5000,
           position: "bottom-center"
+        },
+        login: {
+          label : {
+            text: "login with phone number",
+            DEFAULT : 'login with email & password',
+            PHONE: 'login with phone number'
+          },
+          mode: 'default',
+          DEFAULT : 'default',
+          PHONE: 'phone',
+          OTP: 'otp'
         }
       },
       user: {
         password: null,
         email: null,
+        phone: null,
+        otp: null,
+        login: {
+          data: null
+        }
       },
     }
   },
@@ -85,13 +162,29 @@ export default {
   },
   middleware: 'authenticated',
   methods: {
-    async doLogin() {
-
-      this.button.login.disabled = true;
-
+    confirmOTP(){
+      // this.button.login.disabled = true;
+      try {
+        this.user.login.data.confirm(this.user.otp);
+      }catch (error) {
+        this.$toast.error(error.message, this.options.toast);
+      }
+    },
+    changeLoginMode(){
+      if(this.options.login.mode == this.options.login.PHONE){
+        this.options.login.mode = this.options.login.DEFAULT;
+        this.options.login.label.text = this.options.login.label.PHONE;
+        this.button.login.label = this.button.login.DEFAULT;
+      }else{
+        this.options.login.mode = this.options.login.PHONE;
+        this.options.login.label.text = this.options.login.label.DEFAULT;
+        this.button.login.label = this.button.login.OTP;
+      }
+    },
+    async signInWithEmailAndPassword({email, password}) {
       try {
         let data = await this.$fireAuth.signInWithEmailAndPassword(
-          this.user.email, this.user.password)
+          email, password)
         this.$auth.$storage.setLocalStorage('isLogin', true)
         this.$auth.$storage.setLocalStorage('user', data.user, true)
         await this.$router.push({
@@ -102,6 +195,38 @@ export default {
         this.button.login.disabled = false;
         this.$toast.error(error.message, this.options.toast);
       }
+    },
+    async signInWithPhoneNumber(phone){
+      try {
+        window.confirmationResult = await this.$fireAuth.signInWithPhoneNumber(
+          phone, new this.$fireAuthObj.RecaptchaVerifier('recaptcha-container', {
+            'size': 'invisible',
+            'callback': (response) => {
+              // reCAPTCHA solved, allow signInWithPhoneNumber.
+              console.log("response", response)
+              this.options.login.mode = this.options.login.OTP;
+            }
+          }));
+
+      } catch (error) {
+        this.button.login.disabled = false;
+        this.button.otp.disabled = false;
+        this.$toast.error(error.message, this.options.toast);
+      }
+    },
+    async doLogin() {
+
+      this.button.login.disabled = true;
+
+      if(this.options.login.mode == this.options.login.DEFAULT){
+        await this.signInWithEmailAndPassword({
+          email : this.user.email,
+          password : this.user.password
+        });
+      }else{
+        await this.signInWithPhoneNumber(this.user.phone)
+      }
+
     },
   },
 }
